c450c489b5157060aa1930173619d00665a40a59
Jasael Palma <programadorjp2001@gmail.com>
2025-03-09 14:17:02 -0600

    feat(devcontainer): set up Docker environment for NestJS server

--- server/src/main.ts
+++ server/src/main.ts
@@ -1,8 +1,17 @@
 import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
+import { ConfigService } from '@nestjs/config';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
-  await app.listen(process.env.PORT ?? 3000);
+
+  const configService = app.get(ConfigService);
+  const PORT = configService.get<number>('PORT') ?? 3000;
+
+  await app.listen(PORT);
 }
-bootstrap();
+
+bootstrap().catch((err) => {
+  console.error(err);
+  process.exit(1);
+});
