57d98f0ba11280fde031aab91697b04d8cfc62a2
Jasael Palma <programadorjp2001@gmail.com>
2025-03-09 15:23:05 -0600

    feat(server): implement Users module with controller and service

--- server/src/app.module.ts
+++ server/src/app.module.ts
@@ -2,14 +2,16 @@ import { Module } from '@nestjs/common';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
 import { ConfigModule } from '@nestjs/config';
+import { UsersModule } from './users/users.module';
 
 @Module({
   imports: [
     ConfigModule.forRoot({
       ignoreEnvFile: true,
     }),
+    UsersModule,
   ],
   controllers: [AppController],
   providers: [AppService],
 })
-export class AppModule {}
+export class AppModule { }
