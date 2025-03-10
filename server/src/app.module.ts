ed44203e4a0834b364527258c63a255b7919d20f
Jasael Palma <programadorjp2001@gmail.com>
2025-03-09 18:13:11 -0600

    feat(server): integrate PostgreSQL with TypeORM and update devcontainer configuration

--- server/src/app.module.ts
+++ server/src/app.module.ts
@@ -3,11 +3,22 @@ import { AppController } from './app.controller';
 import { AppService } from './app.service';
 import { ConfigModule } from '@nestjs/config';
 import { UsersModule } from './users/users.module';
+import { TypeOrmModule } from '@nestjs/typeorm';
 
 @Module({
   imports: [
     ConfigModule.forRoot({
       ignoreEnvFile: true,
+    }),
+    TypeOrmModule.forRoot({
+      type: "postgres",
+      host: process.env.POSTGRES_HOST,
+      port: parseInt(process.env.POSTGRES_PORT || '5432'),
+      username: process.env.POSTGRES_USER,
+      password: process.env.POSTGRES_PASSWORD,
+      database: process.env.POSTGRES_DB,
+      entities: [],
+      synchronize: process.env.DB_SYNC === 'true',
     }),
     UsersModule,
   ],
